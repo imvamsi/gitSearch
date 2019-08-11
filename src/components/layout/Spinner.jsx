f18f6da1d1eb89f81fab6ef37a2b7ea3bcf6e199
Vamsikrishna Tamil selvan <vamsikrishnatamilselvan@Vamsikrishnas-MacBook-Air.local>
2019-08-11 17:38:47 -0400

    spinner component added and get request made to github

---
+++ src/components/layout/Spinner.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+import spinner from "./spinner.gif";
+
+const Spinner = () => {
+  return (
+    <Fragment>
+      <img
+        src={spinner}
+        style={{ width: "100px", margin: "auto", display: "block" }}
+      />
+    </Fragment>
+  );
+};
+
+export default Spinner;
